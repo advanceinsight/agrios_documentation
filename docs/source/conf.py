e135fc7bf81d9ef9f93f3312e0786d85aa65a336
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-05 10:44:14 +0100

    Colors dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,10 +45,11 @@ html_theme_options = {
         "color-brand-primary" : "#6b6f76",
         "color-sidebar-caption-text" : "#458A77",
         "color-sidebar-brand-text" : "#458A77",
-        # "color-foreground-primary": "#458A77",
         },
     "dark_css_variables" : {
-        # "color-brand-primary" : "#e17f00"
+        "color-brand-primary" : "#6b6f76",
+        "color-sidebar-caption-text" : "#5FD0B0",
+        "color-sidebar-brand-text" : "#5FD0B0",
         }
     }
 
