4f6940eaee16f00a54379cf65fcc551c5d67102d
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-05 09:46:41 +0100

    Custom colors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'furo'
-# html_static_path = ["_static"]
-# html_css_files = ["custom.css"]
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # FURO CSS
 # https://github.com/pradyunsg/furo/tree/main/src/furo/assets/styles/variables
@@ -45,8 +45,7 @@ html_theme_options = {
         "color-brand-primary" : "#6b6f76",
         "color-sidebar-caption-text" : "#458A77",
         "color-sidebar-brand-text" : "#458A77",
-        "color-foreground-primary": "#458A77",
-        "font-size-h1": "1.4rem",
+        # "color-foreground-primary": "#458A77",
         },
     "dark_css_variables" : {
         # "color-brand-primary" : "#e17f00"
